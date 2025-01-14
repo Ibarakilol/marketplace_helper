df1bc717716c12843d8091b26d72c0f2c48b9691
Nikita <n.lopanov@allfuneral.com>
2025-01-14 16:32:01 +0300

    core settings added

---
+++ app/core/config.py
@@ -0,0 +1,38 @@
+from typing import Literal
+
+from pydantic import PostgresDsn, computed_field
+from pydantic_core import MultiHostUrl
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_ignore_empty=True)
+
+    PROJECT_NAME: str
+    PREFIX: str = "/api/v1"
+    ENVIRONMENT: Literal["dev", "production"] = "dev"
+
+    ALGORITHM = "HS256"
+    SECRET_KEY: str = "changethis"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7  # Week
+
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str = ""
+    POSTGRES_SERVER: str
+    POSTGRES_PORT: int = 5432
+    POSTGRES_DB: str = ""
+
+    @computed_field
+    @property
+    def DATABASE_URI(self) -> PostgresDsn:
+        return MultiHostUrl.build(
+            scheme="postgresql+psycopg",
+            username=self.POSTGRES_USER,
+            password=self.POSTGRES_PASSWORD,
+            host=self.POSTGRES_SERVER,
+            port=self.POSTGRES_PORT,
+            path=self.POSTGRES_DB,
+        )
+
+
+settings = Settings()
